24d3af1eb3d093a75a788dec5bef05b03ff469a2
Sanket <sanketghadmode@gmail.com>
2025-01-08 00:19:50 +0530

    new commit2

---
+++ src/New1/Addition.java
@@ -0,0 +1,14 @@
+package New1;
+
+import java.sql.SQLOutput;
+
+public class Addition {
+
+    public static void main(String[] args) {
+
+        int a = 20;
+        int b = 30;
+        System.out.println(a+b);
+
+    }
+}
