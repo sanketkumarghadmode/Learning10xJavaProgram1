a2a55f565e434b0cb41065441e1a1f795641b766
Sanket <sanketghadmode@gmail.com>
2025-01-08 21:47:39 +0530

    Code with the comment

---
+++ src/LAB1/L2.java
@@ -0,0 +1,19 @@
+package LAB1;
+
+public class L2 {
+
+    public static void main(String[] args) {
+
+        // single line comment
+
+        /* multiline comment
+        * line 1
+        * line 2
+        * line 3
+        *
+        * */
+
+        System.out.println("This is my new L2 code");
+    }
+
+}
