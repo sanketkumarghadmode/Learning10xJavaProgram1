a897438f5be6890109c4559f48778e043fcf0073
sanketkumarghadmode <sanketghadmode@gmail.com>
2025-01-11 11:47:56 +0530

    printed the 9x table using PrintF statement

---
+++ src/all_Labs/PrintF_9X.java
@@ -0,0 +1,24 @@
+package all_Labs;
+
+public class PrintF_9X {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Table of 9 Using PrintF Statement");
+
+        int num = 9;
+        System.out.printf("9*1 = %d\n",num);
+        System.out.printf("9*2 = %d\n",2*num);
+        System.out.printf("9*3 = %d\n",3*num);
+        System.out.printf("9*4 = %d\n",4*num);
+        System.out.printf("9*5 = %d\n",5*num);
+        System.out.printf("9*6 = %d\n",6*num);
+        System.out.printf("9*7 = %d\n",7*num);
+        System.out.printf("9*8 = %d\n",8*num);
+        System.out.printf("9*9 = %d\n",9*num);
+        System.out.printf("9*10 = %d",10*num);
+
+    }
+
+}
