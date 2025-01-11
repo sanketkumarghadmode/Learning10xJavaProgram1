167de23c5d74515367efff477b44905de28be716
sanketkumarghadmode <sanketghadmode@gmail.com>
2025-01-11 16:55:06 +0530

    New feature added

---
+++ src/Newpackageage/DataTypeAndVirables.java
@@ -0,0 +1,20 @@
+package Newpackageage;
+
+public class DataTypeAndVirables {
+
+    public static void main(String[] args) {
+
+
+        boolean b = true;
+        char c = 'A';
+        byte by = 12;
+        short s = 3000;
+        int a = 744798183;
+        long l = 7447818516l;
+        float f = 4.4f;
+        double d = 3.44d;
+
+        System.out.println(b+" "+c+" "+by+" "+s+" "+a+" "+l+" "+f+" "+d);
+
+    }
+}
