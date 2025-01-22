71e15b93638998c5dba22b388f298daa362032ed
sanketkumarghadmode <sanketghadmode@gmail.com>
2025-01-22 19:22:05 +0530

    All operators

---
+++ src/all_Labs/L17UserInput.java
@@ -0,0 +1,17 @@
+package all_Labs;
+
+public class L17UserInput {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Name   : "+args[2]); // 100
+        System.out.println("Age    : "+args[0]); // 101
+        System.out.println("Salary : "+args[1]);
+
+
+
+
+    }
+
+}
---
+++ src/all_Labs/L18UserInputMaxNumberUsingTarnaryOPP.java
@@ -0,0 +1,11 @@
+package all_Labs;
+
+public class L18UserInputMaxNumberUsingTarnaryOPP {
+
+    public static void main(String[] args) {
+
+        System.out.println("fsdf");
+
+
+    }
+}
