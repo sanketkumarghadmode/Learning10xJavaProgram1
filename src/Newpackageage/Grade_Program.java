f23b4ad825205e169f34ae2027eb9fd672df63b8
sanketkumarghadmode <sanketghadmode@gmail.com>
2025-01-22 16:52:03 +0530

    Grade assigne prograame using ternary operators

---
+++ src/Newpackageage/Grade_Program.java
@@ -0,0 +1,24 @@
+package Newpackageage;
+
+public class Grade_Program {
+
+    public static void main(String[] args) {
+
+        /*
+        * A = 90 to 100
+        * B = 80 to 89
+        * C = 70 to 79
+        * D = 60 to 69
+        * F = 0 to 59
+        * */
+
+        int b = 60;
+      String Grades = (b >=90)?"A":(b>=80 && b<=89)?"B":(b>=70 && b<=79)?"C":(b>=60 && b<=69)?"D":"F";
+
+
+        System.out.println(Grades);
+
+
+
+    }
+}
--- src/Newpackageage/InstancesOf_operator.java
+++ src/Newpackageage/InstancesOf_operator.java
@@ -1,4 +1,16 @@
 package Newpackageage;
 
 public class InstancesOf_operator {
+
+    public static void main(String[] args) {
+
+        String src = "new";
+        boolean flag = src instanceof String;
+        System.out.println(flag);
+
+        InstancesOf_operator obj = new InstancesOf_operator();
+        boolean flag2 = obj  instanceof InstancesOf_operator ;
+        
+        
+    }
 }
